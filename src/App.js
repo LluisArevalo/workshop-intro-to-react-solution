8a3fe9a09ae4310a3ecb70d4c99fdd889f18cd8c
LluisArevalo <thorin119@gmail.com>
2018-03-25 22:21:47 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1 className="App-title">Welcome to SpaceX</h1>
+          <img src="/images/logo.png" className="App-logo" alt="SpaceX" />
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;

8e406ddad6b816119558766d8348177954df906e
LluisArevalo <thorin119@gmail.com>
2018-04-02 16:54:06 +0200

    Style the item page

--- src/Components/RocketItem.js
+++ src/Components/RocketItem.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
+import './RocketItem.css';
+
+const apiUrl = 'https://api.spacexdata.com/v2/';
+const capsules = ['dragon1', 'dragon2', 'crewdragon'];
 
 class RocketItem extends Component {
-  render() {
-    const { itemId, handleClick } = this.props;
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  componentDidMount() {
+    const { itemId } = this.props;
+    const isCapsule = capsules.indexOf(itemId) >= 0;
+    const url = `${apiUrl}${ isCapsule ? 'capsules' : 'rockets'}/${itemId}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(item => {
+        this.setState({
+          item,
+          loading: false
+        })
+      });
+  }
+
+  getLoading() {
+    const { handleClick } = this.props;
+    return <button onClick={handleClick}>View all</button>;
+  }
+
+  getInformation() {
+    const { handleClick } = this.props;
+    const { item } = this.state;
 
     return (
-      <article>
-        {itemId}
-        <button onClick={handleClick}>View all</button>
+      <section className="information">
+        <img src={`/images/${item.id}.jpg`} />
+        <div>
+          <h1>{item.name}</h1>
+          <p>{item.description}</p>
+          <button onClick={handleClick}>View all</button>
+        </div>
+      </section>
+    );
+  }
+
+  render() {
+    const { loading } = this.state;
+    const { handleClick } = this.props;
+
+    return (
+      <article className="item-container">
+        { loading ? this.getLoading() : this.getInformation() }
       </article>
     );
   }

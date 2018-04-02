52349fa38e2c1c387e2fa568f3afc02148e5b6fb
LluisArevalo <thorin119@gmail.com>
2018-04-02 17:19:17 +0200

    Add items information to the view

--- src/Components/RocketItem.js
+++ src/Components/RocketItem.js
@@ -35,19 +35,38 @@ class RocketItem extends Component {
 
     return (
       <section className="information">
-        <img src={`/images/${item.id}.jpg`} />
-        <div>
-          <h1>{item.name}</h1>
-          <p>{item.description}</p>
-          <button onClick={handleClick}>View all</button>
-        </div>
+        <img src={`/images/${item.id}.jpg`} alt={item.name} />
+        {this.getDetails(item, item.type)}
+        <button onClick={handleClick}>View all</button>
       </section>
+    );
+  }
+
+  getDetails(data, type) {
+    return (
+      type === 'rocket' ? (
+        <div>
+          <h2>{data.name}</h2>
+          <p>{data.description}</p>
+          <h3>Details</h3>
+          <p><b>Height</b> {data.height.meters} m.</p>
+          <p><b>Diameter</b> {data.diameter.meters} m.</p>
+          <p><b>Mass</b> {data.mass.kg} kg.</p>
+        </div>
+      ) : (
+        <div>
+          <h2>{data.name}</h2>
+          <h3>Details</h3>
+          <p><b>Height (with trunk)</b> {data.height_w_trunk.meters} m.</p>
+          <p><b>Diameter</b> {data.diameter.meters} m.</p>
+          <p><b>Crew capacity</b> {data.crew_capacity} people</p>
+        </div>
+      )
     );
   }
 
   render() {
     const { loading } = this.state;
-    const { handleClick } = this.props;
 
     return (
       <article className="item-container">
